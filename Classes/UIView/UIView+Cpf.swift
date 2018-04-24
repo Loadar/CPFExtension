10eaa47fa5935f9d3563ef70cedbfa38f39ca093
chenpengfei <afeiafeia@163.com>
2018-04-24 10:23:14 +0800

    提供一个添加View Line的方法

--- Classes/UIView/UIView+Cpf.swift
+++ Classes/UIView/UIView+Cpf.swift
@@ -10,11 +10,19 @@ import UIKit
 import CPFChain
 
 extension Cpf where Base: UIView {
-    // MARK: - Line
+    // MARK: - Layer Line
     @discardableResult
     public func addLine(width: CGFloat? = nil, color: UIColor, from: CGPoint, to: CGPoint) -> Cpf {
         base.cpf_addLine(width: width, color: color, from: from, to: to)
         return self
     }
 
+    // MARK: - View Line
+    public func addLine(width: CGFloat? = nil, color: UIColor, configure closure: (UIView) -> Void) {
+        let line = UIView()
+        line.backgroundColor = color
+        line.isUserInteractionEnabled = false
+        base.addSubview(line)
+        closure(line)
+    }
 }
