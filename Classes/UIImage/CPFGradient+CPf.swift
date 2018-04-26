5533ca58fbd48008dde52c3eae04aabd779a322c
chenpengfei <afeiafeia@163.com>
2018-04-26 11:26:58 +0800

    修改gradient image的使用方式

---
+++ Classes/UIImage/CPFGradient+CPf.swift
@@ -0,0 +1,79 @@
+//
+//  CPFGradient+CPf.swift
+//  CPFExtensions
+//
+//  Created by Aaron on 2018/4/26.
+//  Copyright © 2018年 ruhnn. All rights reserved.
+//
+
+import UIKit
+import CPFChain
+
+extension Cpf where Base: CPFGradient {
+    @discardableResult
+    public func style(_ style: CPFGradient.Style) -> Cpf {
+        base.style = style
+        return self
+    }
+    
+    @discardableResult
+    public func size(_ size: CGSize) -> Cpf {
+        base.size = size
+        return self
+    }
+    
+    @discardableResult
+    public func colors(_ colors: [UIColor]) -> Cpf {
+        base.colors = colors
+        return self
+    }
+    
+    @discardableResult
+    public func locations(_ locations: [Double]) -> Cpf {
+        base.locations = locations
+        return self
+    }
+    
+    @discardableResult
+    public func start(_ start: CGPoint) -> Cpf {
+        base.start = start
+        return self
+    }
+    
+    @discardableResult
+    public func end(_ end: CGPoint) -> Cpf {
+        base.end = end
+        return self
+    }
+}
+
+extension Cpf where Base: UITextField {
+    @discardableResult
+    public func background(radient: CPFGradient, state: UIControlState? = nil) -> Cpf {
+        let image = radient.image
+        if state == .disabled {
+            base.disabledBackground = image
+        } else {
+            base.background = image
+        }
+        return self
+    }
+
+    @discardableResult
+    public func background(radient: Cpf<CPFGradient>, state: UIControlState? = nil) -> Cpf {
+        return self.background(radient: radient.base, state: state)
+    }
+}
+
+extension Cpf where Base: UIButton {
+    @discardableResult
+    public func background(radient: CPFGradient, state: UIControlState = .normal) -> Cpf {
+        base.setBackgroundImage(radient.image, for: state)
+        return self
+    }
+    
+    @discardableResult
+    public func background(radient: Cpf<CPFGradient>, state: UIControlState = .normal) -> Cpf {
+        return self.background(radient: radient.base, state: state)
+    }
+}
---
+++ Classes/UIImage/CPFGradient.swift
@@ -0,0 +1,36 @@
+//
+//  CPFGradient.swift
+//  CPFExtensions
+//
+//  Created by Aaron on 2018/4/26.
+//  Copyright © 2018年 ruhnn. All rights reserved.
+//
+
+import UIKit
+
+public class CPFGradient {
+    public var size: CGSize = .zero
+    public var colors: [UIColor] = []
+    public var locations: [Double]? = nil
+    
+    public var start: CGPoint? = nil
+    public var end: CGPoint? = nil
+    
+    public var style: CPFGradient.Style = .liner
+    
+    public var image: UIImage? {
+        switch style {
+        case .liner:
+            return UIImage.cpf_gradient(size: size, colors: colors, locations: locations, start: start, end: end)
+        case .radial:
+            return UIImage.cpf_radialGradient(size: size, colors: colors, locations: locations)
+        }
+    }
+}
+
+extension CPFGradient {
+    public enum Style {
+        case liner
+        case radial
+    }
+}
