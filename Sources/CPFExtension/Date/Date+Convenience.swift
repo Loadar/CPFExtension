791cbe04d60ce82649843ec4dabc25eb14db2a36
ChenPengfei <pengfei@zhiyitech.cn>
2023-06-06 15:31:30 +0800

    Date增加便利扩展

---
+++ Sources/CPFExtension/Date/Date+Convenience.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aaron on 2023/6/6.
+//
+
+import Foundation
+
+public extension Date {
+    /// 一天的秒数(60 * 60 * 24s)，方便用于计算
+    static var cpfSecondsOfOneDay: TimeInterval {
+        self.cpf.secondsOfOneDay
+    }
+    
+    /// 昨天
+    static var cpfYesterday: Date {
+        self.cpf.yesterday
+    }
+    
+    /// 今天
+    static var cpfToday: Date {
+        self.cpf.today
+    }
+    
+    /// 明天
+    static var cpfTommorow: Date {
+        self.cpf.tommorow
+    }
+}
